d2b4612262ed7dcca0c021490313031821866574
Tim Parrish <timothyjparrish@gmail.com>
2019-09-04 18:33:06 -0600

    Initial setup of repo

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Future home of Tim, Alex & Ransom final project for Fall 2019 Web Development course @ MSU!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
