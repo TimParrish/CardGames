b3413d128a1b0c693337b8e96e0e33ea2eaa43bd
Tim Parrish <timothyjparrish@gmail.com>
2019-11-15 08:42:39 -0700

    made homepage images into links

--- src/GameComponents/Homepage.js
+++ src/GameComponents/Homepage.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { HomepageStyle } from "styles";
 import { Casino, Tank } from "images";
+import { NavLink } from "react-router-dom";
 
 function Homepage() {
   return (
     <>
       <HomepageStyle>
         <h1>Please select a game!</h1>
-        <img src={Casino} alt={"Neon casino marquee sign at night"} />
-        <img
-          src={Tank}
-          alt={"Tank on a dirt road with the sky in the background"}
-        />
+        {/* <a href="https://timparrish.github.io/card-games/blackjack"> */}
+        <NavLink to="/card-games/blackjack">
+          <img src={Casino} alt={"Neon casino marquee sign at night"} />
+        </NavLink>
+        <NavLink to="/card-games/war">
+          <img
+            src={Tank}
+            alt={"Tank on a dirt road with the sky in the background"}
+          />
+        </NavLink>
       </HomepageStyle>
     </>
   );
