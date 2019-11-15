2bc65a1d4537be245db3ee389af057634c29439a
aackerlund <alexackerlund@Bresnan.net>
2019-11-15 12:15:54 -0700

    Quick fix for image file paths

--- src/images/index.js
+++ src/images/index.js
@@ -1,3 +1,3 @@
 export { default as CardsOnFire } from "./julius-drost-C8wlYF8ubBo-unsplash.jpg";
-export { default as Casino } from "./michal-soukup-k2OGxffv3VY-unsplash";
-export { default as Tank } from "./justin-campbell-FacLsqkeX4w-unsplash";
+export { default as Casino } from "./michal-soukup-k2OGxffv3VY-unsplash.jpg";
+export { default as Tank } from "./justin-campbell-FacLsqkeX4w-unsplash.jpg";
