2325e0696d2d97e3d6f9b7ae793e581eb7a1fe23
aackerlund <alexackerlund@Bresnan.net>
2019-11-18 13:31:55 -0700

    Added onLoad type capabilities to get a new deck when the War game page is loaded

--- src/GameComponents/War.js
+++ src/GameComponents/War.js
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+import React, { 
+  useState,
+  useEffect
+ } from "react";
 import axios from "axios";
 import {
   DisplayHand,
@@ -23,6 +26,11 @@ function War() {
   const hiddenCardImage = CardsOnFire;
   let curDeck;
   void shuffled;
+
+  //calls startNewGame() when th page loads
+  useEffect(() => {
+    startNewGame();
+  }, []);
 
   // Create a new deck of cards and store the deck id for future API calls
   function startNewGame() {
